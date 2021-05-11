d21a497078bb78f9fe81be8fce0a8ff8d4c50d66
Streds <laurenstredler@MacBook-Pro.local>
2021-05-10 21:41:24 -0500

    updates

---
+++ Missions_to_Mars/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+from splinter import Browser
+from pprint import pprint
+import pymongo
+import pandas as pd
+import requests
+from flask import Flask, render_template
+import time
+import numpy as np
+import json
+from selenium import webdriver
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", data=mars_data)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
